669c2a71bee96321ba0a86da4f40f6e94ba031e7
taknov26 <tak.nov26@gmail.com>
2021-03-27 01:48:41 +0000

    テストパス、微修正済み

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,6 +7,12 @@ class Admins::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    if @order.orders_status == 1
+        @order.orders_items.each do |orders_item|
+          orders_item.make_status = 1
+          orders_item.save
+        end
+    end
     redirect_to admins_order_path(@order)
   end
 
--- app/controllers/admins/orders_items_controller.rb
+++ app/controllers/admins/orders_items_controller.rb
@@ -2,7 +2,16 @@ class Admins::OrdersItemsController < ApplicationController
 
   def update
     @orders_item = OrdersItem.find(params[:id])
+    @order = @orders_item.order
+    @orders_items = @order.orders_items
     @orders_item.update(orders_item_params)
+      if @orders_item.make_status == 2
+        @orders_item.order.orders_status = 2
+        @orders_item.order.save
+      elsif @orders_items.where(make_status: [3]).count == @orders_items.count
+        @orders_item.order.orders_status = 3
+        @orders_item.order.save
+      end
     redirect_to admins_order_path(@orders_item.order_id)
   end
 
@@ -11,3 +20,5 @@ class Admins::OrdersItemsController < ApplicationController
     params.require(:orders_item).permit(:make_status)
   end
 end
+
+#
